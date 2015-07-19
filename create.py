c8952ea56fd7ded4534750a273f646ae0efce084
keleger <greferry@gmail.com>
2015-07-19 18:02:10 +0800

    content of python source file modified

--- create.py
+++ create.py
@@ -27,10 +27,12 @@ def create_new_problem(problem_name):
 
 ---
 
-代码实现：[%s](./%s.py)
-
 ---
+
+代码实现：[%s](./%s.py)
 	''' % (problem_name, url_name, problem_name, short_name)
+
+
 	md_file.write(md_content)
 	md_file.close()
 
