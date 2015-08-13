e5e380c8678003a1c387df9d456c4933209687c5
keleger <greferry@gmail.com>
2015-08-13 14:12:29 +0800

    correct format

--- algorithms/3SumClosest/3SumClosest.py
+++ algorithms/3SumClosest/3SumClosest.py
@@ -11,31 +11,31 @@ class Solution:
     # @param {integer} target
     # @return {integer}
     def threeSumClosest(self, nums, target):
-    	nums.sort()
-    	length = len(nums)
-    	min_result = nums[0] + nums[1] + nums[2]
+        nums.sort()
+        length = len(nums)
+        min_result = nums[0] + nums[1] + nums[2]
 
-    	for i in range(length - 2):
-    		j, k = i+1, length-1
-    		while j < k:
-    			sum_result = nums[i] + nums[j] + nums[k]
+        for i in range(length - 2):
+            j, k = i+1, length-1
+            while j < k:
+                sum_result = nums[i] + nums[j] + nums[k]
 
-    			if sum_result == target:
-    				return target
+                if sum_result == target:
+                    return target
 
-    			if abs(sum_result - target) < abs(min_result - target):
-    				min_result = sum_result
+                if abs(sum_result - target) < abs(min_result - target):
+                    min_result = sum_result
 
-    			if sum_result > target:
-    				k -= 1
-    			else:
-    				j += 1
+                if sum_result > target:
+                    k -= 1
+                else:
+                    j += 1
 
-    	return min_result
+        return min_result
 
 if __name__ == '__main__':
-	s = Solution()
-	nums = [-1, 2, 1, -4]
-	target = 1
-	print s.threeSumClosest(nums, target)
+    s = Solution()
+    nums = [-1, 2, 1, -4]
+    target = 1
+    print s.threeSumClosest(nums, target)
 
