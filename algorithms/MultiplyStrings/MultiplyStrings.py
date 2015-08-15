69cd5c9fbcbb1346944ce2a09c7bea97d2ee5917
keleger <greferry@gmail.com>
2015-08-15 10:32:24 +0800

    create MultiplyStrings

--- algorithms/MultiplyStrings/MultiplyStrings.py
+++ algorithms/MultiplyStrings/MultiplyStrings.py
@@ -5,3 +5,10 @@ github: https://github.com/gitferry
 author: Fangyu Gai <gaigai508@gmail.com>
 date  : 15/08/2015
 '''
+
+class Solution:
+    # @param {string} num1
+    # @param {string} num2
+    # @return {string}
+    def multiply(self, num1, num2):
+        return str(int(num1) * int(num2))
