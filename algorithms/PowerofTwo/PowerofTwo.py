648c6eaf2962fb5a51b074614de37aefc038ebb6
keleger <greferry@gmail.com>
2015-10-10 08:32:47 +0800

    forget to save changes

--- algorithms/PowerofTwo/PowerofTwo.py
+++ algorithms/PowerofTwo/PowerofTwo.py
@@ -5,3 +5,13 @@ github: https://github.com/gitferry
 author: Fangyu Gai <gaigai508@gmail.com>
 date  : 09/10/2015
 '''
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        return n > 0 and not n & (n - 1)
+
